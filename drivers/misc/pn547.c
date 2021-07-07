e6b844e0a0a5cde5ffa70de5895d300a3c6633b3
Danny Trunk <dtrunk90@gmail.com>
2021-07-07 16:59:36 +0200

    Revert "drivers: misc: pn54x: Fix frame size for 32bits"
    This reverts commit e69152a8845d218af55e967da4004e4166a36572.
    
    Change-Id: Iae907bdbd998ab982d64a422cf568450a5ee53de

--- drivers/misc/pn547.c
+++ drivers/misc/pn547.c
@@ -48,13 +48,9 @@
 #include <linux/clk.h>
 #endif
 
+#define MAX_BUFFER_SIZE		512
 #define MAX_NORMAL_FRAME_SIZE	(255 + 3)
-
-#if defined(CONFIG_ARM64) || defined(CONFIG_64BIT)
-#define MAX_FRAME_SIZE	(1023 + 5)
-#else
-#define MAX_FRAME_SIZE	512
-#endif
+#define MAX_FIRMDL_FRAME_SIZE	(1023 + 5)
 
 #define NFC_DEBUG 0
 #define MAX_TRY_I2C_READ	10
@@ -160,7 +156,7 @@ static ssize_t pn547_dev_read(struct file *filp, char __user *buf,
 			      size_t count, loff_t *offset)
 {
 	struct pn547_dev *pn547_dev = filp->private_data;
-	char tmp[MAX_FRAME_SIZE] = {0, };
+	char tmp[MAX_FIRMDL_FRAME_SIZE] = {0, };
 	int ret = 0, maxlen;
 	int readingWatchdog = 0;
 	bool fwdl;
@@ -169,12 +165,12 @@ static ssize_t pn547_dev_read(struct file *filp, char __user *buf,
 
 	fwdl = pn547_dev->state == PN547_STATE_FWDL;
 	if (nfc_has_pinctrl) {
-		maxlen = fwdl ? MAX_FRAME_SIZE : MAX_NORMAL_FRAME_SIZE;
+		maxlen = fwdl ? MAX_FIRMDL_FRAME_SIZE : MAX_NORMAL_FRAME_SIZE;
 		if (count > maxlen)
 			count = maxlen;
 	} else {
-		if (count > MAX_FRAME_SIZE)
-			count = MAX_FRAME_SIZE;
+		if (count > MAX_BUFFER_SIZE)
+			count = MAX_BUFFER_SIZE;
 	}
 
 	pr_debug("%s : reading %zu bytes. irq=%s\n", __func__, count,
@@ -261,7 +257,7 @@ static ssize_t pn547_dev_write(struct file *filp, const char __user *buf,
 			       size_t count, loff_t *offset)
 {
 	struct pn547_dev *pn547_dev;
-	char tmp[MAX_FRAME_SIZE] = {0, };
+	char tmp[MAX_FIRMDL_FRAME_SIZE] = {0, };
 	int ret = 0, retry = 5, maxlen;
 	bool fwdl;
 
@@ -273,12 +269,12 @@ static ssize_t pn547_dev_write(struct file *filp, const char __user *buf,
 
 	fwdl = pn547_dev->state == PN547_STATE_FWDL;
 	if (nfc_has_pinctrl) {
-		maxlen = fwdl ? MAX_FRAME_SIZE : MAX_NORMAL_FRAME_SIZE;
+		maxlen = fwdl ? MAX_FIRMDL_FRAME_SIZE : MAX_NORMAL_FRAME_SIZE;
 		if (count > maxlen)
 			count = maxlen;
 	} else {
-		if (count > MAX_FRAME_SIZE)
-			count = MAX_FRAME_SIZE;
+		if (count > MAX_BUFFER_SIZE)
+			count = MAX_BUFFER_SIZE;
 	}
 
 	if (copy_from_user(tmp, buf, count)) {
